2532140e96aaa8e2604670b710bb083fe29f5c10
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-06-12 00:03:41 +0530

    Update Circle.java

--- Codes/Class Object Concept/Circle.java
+++ Codes/Class Object Concept/Circle.java
@@ -4,6 +4,10 @@ import java.text.DecimalFormat;
 
 public class Circle {
     
+    private static final DecimalFormat zero = new DecimalFormat("0.00");
+    private double radius;
+    private String colour;
+            
 
 
 }
