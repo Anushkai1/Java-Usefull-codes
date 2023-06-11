6f9692d6a7084ae33d8f6f06c3e98189cae9854a
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-06-12 00:01:59 +0530

    Update Regex Testing.java

--- Codes/Regex Testing.java
+++ Codes/Regex Testing.java
@@ -3,5 +3,18 @@
         String st = textFld.getText();
                char d = evt.getKeyChar();
        int len = st.length();
-
+        if(((len <10 && Character.isDigit(d) ) || (d==KeyEvent.CHAR_UNDEFINED  || d==KeyEvent.VK_BACK_SPACE || d==KeyEvent.VK_DELETE || d=='' || d=='' || d=='' || d==''))){
+           // System.out.println(" *****");
+            evt.consume();
+        }
+        int intg = -2;
+        try{
+            intg = Integer.parseInt(st);
+        }catch(Exception e){}
+      
+        if(intg!=-2 && 400000000<intg && intg<800000000){
+            textFld.setVldInput(true);//textFld.repaint();
+        }else{
+            textFld.setVldInput(false);//textFld.repaint();
+        } 
     }  
