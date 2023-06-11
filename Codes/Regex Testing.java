533f445b981c10c8762d1fee79a55109a192992a
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-06-12 00:01:51 +0530

    Create Regex Testing.java

---
+++ Codes/Regex Testing.java
@@ -0,0 +1,7 @@
+ public static void rxPhone(java.awt.event.KeyEvent evt) {                                    
+        TxFld1 textFld = (TxFld1) evt.getSource();
+        String st = textFld.getText();
+               char d = evt.getKeyChar();
+       int len = st.length();
+
+    }  
