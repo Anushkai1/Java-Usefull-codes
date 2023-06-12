19069cc34ff8ce38c7792bb7092e6bd8f4bdd227
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-06-13 00:06:00 +0530

    Update TestCircle.java

--- Codes/Class Object Concept/TestCircle.java
+++ Codes/Class Object Concept/TestCircle.java
@@ -1,6 +1,12 @@
 package Exercise;
 
 public class TestCircle {
+    public static void main(String[] args){
+        Circle circle1 = new Circle();
+        circle1.setRaduis(10.1);
+        circle1.setColour("Brown");
+        
 
+    }
 }
 
