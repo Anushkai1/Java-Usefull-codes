c15e6c4a8f1741e5b4c09a230fa990c4e2d3ff28
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-06-13 00:08:59 +0530

    Update Player.java

--- Codes/Class Object Concept/Player.java
+++ Codes/Class Object Concept/Player.java
@@ -2,5 +2,16 @@
 package obkects;
 
 public class Player {
+    String title;
+    int id ;
+    double length;
+    
+    //default non parameterized constructor
+    Player(){
+        this.id = 1;
+        this.title = "Loading";
+        this.length = .5;
+    }
+    
 
 }
