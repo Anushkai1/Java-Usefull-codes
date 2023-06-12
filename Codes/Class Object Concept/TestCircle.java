dc81855dccfd07f445b560835aa8406617f09e03
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-06-13 00:06:08 +0530

    Update TestCircle.java

--- Codes/Class Object Concept/TestCircle.java
+++ Codes/Class Object Concept/TestCircle.java
@@ -6,7 +6,15 @@ public class TestCircle {
         circle1.setRaduis(10.1);
         circle1.setColour("Brown");
         
-
+        circle1.getPerimeter();
+        circle1.getArea();
+        
+        Circle circle2 = new Circle();
+        circle2.setRaduis(15.58);
+        circle2.setColour("Pink");
+        
+        circle2.getPerimeter();
+        circle2.getArea();
     }
 }
 
