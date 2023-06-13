62e102c19dfa9db5036485a80610b41d1992e2be
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-06-14 00:11:49 +0530

    Update Object.java

--- Codes/Class Object Concept/Object.java
+++ Codes/Class Object Concept/Object.java
@@ -7,7 +7,14 @@ public class Object {
         Player p1 = new Player();
         p1.reprt();
         
-
+        Player p2 = new Player(2, "sdf", 2.5);
+        p2.reprt();
+      
+        Player p3 = new Player(2, "sdf");
+        p3.reprt();
+              
+        Player p4 = new Player(2, 3);
+        p4.reprt();
                   
     }
     
