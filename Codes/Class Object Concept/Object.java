f7a62d55cc709f69a6565028aab11620f743ec9f
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-06-14 00:11:38 +0530

    Update Object.java

--- Codes/Class Object Concept/Object.java
+++ Codes/Class Object Concept/Object.java
@@ -3,7 +3,13 @@ package obkects;
 
 public class Object {
 
+    public static void main(String[] args) {
+        Player p1 = new Player();
+        p1.reprt();
+        
 
+                  
+    }
     
 
     
