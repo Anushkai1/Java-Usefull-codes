5e5a9ac269855c890ee01677dbacf3e33185d7f7
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-06-15 00:18:12 +0530

    Update Get User Input.java

--- Codes/Get User Input.java
+++ Codes/Get User Input.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class Exerxise_2 {
 
     public static void main(String[] args) {
+        int a , total=0;
+        Scanner sc = new Scanner(System.in);
+        
+        for(int i = 0; i<5; i++){
+            System.out.print("Enter an integer number\t:\t");
+            total+=sc.nextInt();
+        }
+
 
     }
     
